bb0cdff2c616e750fc550af20ec9ef81903975f6
Jonghui <68373532+hie6953@users.noreply.github.com>
2023-01-09 09:49:14 +0900

    #3 - chore: 프로젝트 초기 환경 생성

---
+++ server/matp/src/main/java/com/matp/MatpApplication.java
@@ -0,0 +1,13 @@
+package com.matp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatpApplication.class, args);
+	}
+
+}
