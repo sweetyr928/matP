b24fcf72411d002ef0a8c569fc7c2eb12634200a
h1em0n1m <h1em0n1m@gmail.com>
2023-01-11 15:39:40 +0900

    #8 - feat : 맛픽커즈 create 기능 구현

--- client/src/components/MatPickerModal/MatPickerUpdate.tsx
+++ client/src/components/MatPickerModal/MatPickerUpdate.tsx
@@ -1,5 +1,60 @@
-const MatPickerUpdate = () => {
-  return <div></div>;
+/* eslint-disable */
+
+import React, { PropsWithChildren } from "react";
+import styled from "styled-components";
+
+const ModalContainer = styled.div`
+  width: 411px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 20px;
+  left: 62px;
+`;
+
+const DialogBox = styled.dialog`
+  width: 370px;
+  height: 440px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: none;
+  border-radius: 3px;
+  box-sizing: border-box;
+  background-color: white;
+  z-index: 10000;
+`;
+
+const Backdrop = styled.div`
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
+
+interface ModalDefaultType {
+  onClickToggleModal: () => void;
+}
+
+const MatPickerUpdate = ({
+  onClickToggleModal,
+}: PropsWithChildren<ModalDefaultType>) => {
+  return (
+    <ModalContainer>
+      <DialogBox></DialogBox>
+      <Backdrop
+        onClick={(e: React.MouseEvent) => {
+          e.preventDefault();
+          if (onClickToggleModal) {
+            onClickToggleModal();
+          }
+        }}
+      />
+    </ModalContainer>
+  );
 };
 
 export default MatPickerUpdate;
--- client/src/components/MatPickersList.tsx
+++ client/src/components/MatPickersList.tsx
@@ -1,11 +1,48 @@
 /* eslint-disable */
+import { useState, useCallback } from "react";
 import styled from "styled-components";
+import MatPickerUpdate from "./MatPickerModal/MatPickerUpdate";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const MatPickerSingleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
   height: 50px;
   padding: 15px;
   border-bottom: 1px solid black;
+`;
+
+const NameBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 300px;
+  button {
+    background-color: transparent;
+  }
+  .icon {
+    width: 30px;
+    height: 30px;
+    border: 1px solid black;
+    border-radius: 50%;
+    margin-right: 20px;
+    background-color: ${(props) => props.color || "gray"};
+  }
+`;
+
+const ButtonBox = styled.div`
+  * {
+    margin-right: 8px;
+  }
+  .update_btn_hidden {
+    display: none;
+  }
+  .delete_btn_hidden {
+    display: none;
+  }
 `;
 
 interface PickersProps {
@@ -15,9 +52,37 @@ interface PickersProps {
 }
 
 const MatPickersList = ({ picker }: { picker: PickersProps }) => {
+  const [isOpenModal, setOpenModal] = useState<boolean>(false);
+
+  const onClickToggleModal = useCallback(() => {
+    setOpenModal(!isOpenModal);
+  }, [isOpenModal]);
+
   return (
     <>
-      <MatPickerSingleBox></MatPickerSingleBox>
+      <MatPickerSingleBox>
+        {isOpenModal && (
+          <MatPickerUpdate onClickToggleModal={onClickToggleModal} />
+        )}
+        <NameBox color={picker.color}>
+          <div className="icon"></div>
+          <div>{picker.name}</div>
+        </NameBox>
+        <ButtonBox>
+          <EditIcon
+            className={`update_btn${
+              picker.name === "기본 맛픽커즈" ? "_hidden" : ""
+            }`}
+            onClick={onClickToggleModal}
+          />
+
+          <DeleteIcon
+            className={`delete_btn${
+              picker.name === "기본 맛픽커즈" ? "_hidden" : ""
+            }`}
+          />
+        </ButtonBox>
+      </MatPickerSingleBox>
     </>
   );
 };
