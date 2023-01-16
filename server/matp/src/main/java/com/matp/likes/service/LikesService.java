bfa2f5691d173f06d72853412b93d5ed8893c66f
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2023-01-16 17:29:44 +0900

    #24 - feat : LikesService 생성
    # : likes 를 저장하고 update 쿼리로 post의 likes 숫자를 +1 함

---
+++ server/matp/src/main/java/com/matp/likes/service/LikesService.java
@@ -0,0 +1,36 @@
+package com.matp.likes.service;
+
+
+import com.matp.likes.dto.LikesRequest;
+import com.matp.likes.entity.Likes;
+import com.matp.likes.repository.LikesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class LikesService {
+
+    private final LikesRepository likesRepository;
+
+
+    @Transactional
+    public Mono<Void> increaseLikes(LikesRequest likeRequest, Long postId, Long memberId) {
+
+        Likes likes = likeRequest.toEntity();
+        likes.settingLikes(memberId,postId,likeRequest.likesCheck());
+
+        return likesRepository.save(likes)
+                .then(likesRepository.increasePostLikesCount(postId));
+
+    }
+    @Transactional
+    public Mono<Void> decreaseLikes(Long postId, Long memberId) {
+        //TODO 멤버 검증로직 들어가야함 .
+
+        return likesRepository.findLikes(postId, memberId).flatMap(likesRepository::delete)
+                .then(likesRepository.decreasePostLikesCount(postId));
+    }
+}
