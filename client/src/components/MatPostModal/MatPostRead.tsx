da215386bb921fcf107b8d6e9c5c473e9dc235ba
sweetyr928 <031694@naver.com>
2023-01-17 14:18:21 +0900

    #22 - feat: post modal StyledContent min/max height 추가

--- client/src/components/MatPostModal/MatPostRead.tsx
+++ client/src/components/MatPostModal/MatPostRead.tsx
@@ -95,14 +95,12 @@ const StyledInfo = styled.div`
 const StyledContent = styled.div`
   margin: 0px 0px 5px 0px;
   padding: 1px 0px 0px 0px;
-  max-height: 300px;
+  min-height: 270px;
+  max-height: 270px;
   overflow-y: scroll;
 
   &::-webkit-scrollbar {
     display: none;
-  }
-
-  .p {
   }
 `;
 
