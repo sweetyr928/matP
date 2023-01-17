93b0b82f98c0e1be1ee8c43afbc366391000e63a
Rhino-ty <okko8522@gmail.com>
2023-01-17 15:03:12 +0900

    #2 - feat: 마커에 클릭을 한다면 인포윈도우가 하나만 나오게 기능 추가
    useState로 인덱스 별로 조정해 하나만 나오게 수정

--- client/src/components/KakaoMap/MapMarkerComponent.tsx
+++ client/src/components/KakaoMap/MapMarkerComponent.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { MapMarker } from "react-kakao-maps-sdk";
 import styled from "styled-components";
-// import { useRecoilValue } from "recoil";
-// import { searchResultsState } from "../../store/searchAtoms";
+import { useRecoilValue } from "recoil";
+import { searchResultsState } from "../../store/searchAtoms";
 
 const InfoWindowContainer = styled.div`
   min-width: 150px;
@@ -13,59 +13,93 @@ const InfoValue = styled.div`
 `;
 
 const MapMarkerComponent = () => {
-  // const searchResults = useRecoilValue(searchResultsState);
+  const searchResults = useRecoilValue(searchResultsState);
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState({
+    id: -1,
+    open: false,
+  });
+
+  // return (
+  // <MapMarker // 마커를 생성합니다
+  //   position={{
+  //     // 마커가 표시될 위치
+  //     lat: 37.50039427271689,
+  //     lng: 127.02796438287635,
+  //   }}
+  //   clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정
+  //   image={{
+  //     src: "https://cdn-icons-png.flaticon.com/512/7988/7988273.png", // 마커이미지의 주소입니다
+  //     size: {
+  //       width: 30,
+  //       height: 30,
+  //     }, // 마커이미지의 크기입니다
+  //   }}
+  //   onClick={() => setIsOpen(true)}
+  // >
+  //   {isOpen && (
+  //     <InfoWindowContainer onClick={() => setIsOpen(false)}>
+  //       {/* <img
+  //           alt="close"
+  //           width="14"
+  //           height="13"
+  //           src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
+  //           style={{
+  //             position: "absolute",
+  //             right: "5px",
+  //             top: "5px",
+  //             cursor: "pointer",
+  //           }}
+  //         /> */}
+  //       <InfoValue>땀땀</InfoValue>
+  //     </InfoWindowContainer>
+  //   )}
+  // </MapMarker>
+  {
+    /* 검색 데이터 오면 쓸 map 함수 */
+  }
+  // <>
+  // {searchResults.map((result) => (
+  //   <MapMarker
+  //     key={result.id}
+  //     position={{ lat: result.latitude, lng: result.longitude }}
+  //     clickable={true}
+  //     image={{
+  //       src: "https://cdn-icons-png.flaticon.com/512/7988/7988273.png",
+  //       size: { width: 30, height: 30 },
+  //     }}
+  //     onClick={() => setIsOpen(true)}
+  //   >
+  //     {isOpen && (
+  //       <InfoWindowContainer onClick={() => setIsOpen(false)}>
+  //         <InfoValue>{result.placeName}</InfoValue>
+  //       </InfoWindowContainer>
+  //     )}
+  //   </MapMarker>
+  // ))}
+  // </>
+  // );
   return (
-    <MapMarker // 마커를 생성합니다
-      position={{
-        // 마커가 표시될 위치
-        lat: 37.50039427271689,
-        lng: 127.02796438287635,
-      }}
-      clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정
-      image={{
-        src: "https://cdn-icons-png.flaticon.com/512/7988/7988273.png", // 마커이미지의 주소입니다
-        size: {
-          width: 30,
-          height: 30,
-        }, // 마커이미지의 크기입니다
-      }}
-      onClick={() => setIsOpen(true)}
-    >
-      {isOpen && (
-        <InfoWindowContainer onClick={() => setIsOpen(false)}>
-          {/* <img
-              alt="close"
-              width="14"
-              height="13"
-              src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
-              style={{
-                position: "absolute",
-                right: "5px",
-                top: "5px",
-                cursor: "pointer",
-              }}
-            /> */}
-          <InfoValue>땀땀</InfoValue>
-        </InfoWindowContainer>
-      )}
-      {/* 검색 데이터 오면 쓸 map 함수 */}
-      {/* {searchResults.map((result) => (
+    <>
+      {searchResults.map((result) => (
         <MapMarker
           key={result.id}
           position={{ lat: result.latitude, lng: result.longitude }}
           clickable={true}
-          onClick={() => setIsOpen(true)}
+          image={{
+            src: "https://cdn-icons-png.flaticon.com/512/7988/7988273.png",
+            size: { width: 30, height: 30 },
+          }}
+          onClick={() => setIsOpen({ id: result.id, open: true })}
         >
-          {isOpen && (
-            <InfoWindowContainer onClick={() => setIsOpen(false)}>
-              <InfoValue>{result.name}</InfoValue>
+          {isOpen.open && isOpen.id === result.id && (
+            <InfoWindowContainer onClick={() => setIsOpen({ id: -1, open: false })}>
+              <InfoValue>{result.placeName}</InfoValue>
             </InfoWindowContainer>
           )}
         </MapMarker>
-      ))} */}
-    </MapMarker>
+      ))}
+    </>
   );
 };
 
