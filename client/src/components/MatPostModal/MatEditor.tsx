4762dd9f86920dc702886517d05c26283751ac9c
sweetyr928 <031694@naver.com>
2023-01-18 20:00:33 +0900

    #41 - feat : image handling 함수 구현

--- client/src/components/MatPostModal/MatEditor.tsx
+++ client/src/components/MatPostModal/MatEditor.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable */
 
-import React, { useRef, useState, useMemo } from "react";
+import React, { useRef, useMemo } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import ImageResize from "@looop/quill-image-resize-module-react";
+import axios, { AxiosError } from "axios";
 
 Quill.register("modules/imageResize", ImageResize);
 
@@ -16,88 +17,58 @@ const MatEditor = ({ htmlContent, setHtmlContent }: QuillEditorProps) => {
   const QuillRef = useRef<ReactQuill>();
 
   /**
-   *  이미지를 업로드 하기 위한 함수
+   *  이미지 핸들러(modules 설정보다 위에 있어야 정상 적용)
    */
-  // const imageHandler = () => {
-  // 이미지 업로드 핸들러, modules 설정보다 위에 있어야 정상 적용
-  //   const imageHandler = () => {
-  //     // file input 임의 생성
-  //     const input = document.createElement('input');
-  //     input.setAttribute('type', 'file');
-  //     input.click();
+  const imageHandler = () => {
+    // 파일을 업로드 하기 위한 input 태그 생성
+    const input = document.createElement("input");
+    const formData = new FormData();
+    let url = "";
 
-  //     input.onchange = async() => {
-  //         const file = input.files;
-  //         const formData = new FormData();
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
 
-  //         if(file) {
-  //             formData.append("multipartFiles", file[0]);
-  //         }
+    // 파일이 input 태그에 담기면 실행 될 함수
+    input.onchange = async () => {
+      const file = input.files;
 
-  //         // file 데이터 담아서 서버에 전달하여 이미지 업로드
-  //         const res = await axios.post('http://localhost:8080/uploadImage', formData);
+      if (file) {
+        formData.append("multipartFiles", file[0]);
+      }
 
-  //         if(quillRef.current) {
-  //             // 현재 Editor 커서 위치에 서버로부터 전달받은 이미지 불러오는 url을 이용하여 이미지 태그 추가
-  //             const index = (quillRef.current.getEditor().getSelection() as RangeStatic).index;
+      try {
+        // file 데이터 담아서 서버에 전달하여 이미지 업로드
+        const res = await axios.post("서버 배포 url/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
 
-  //             const quillEditor = quillRef.current.getEditor();
-  //             quillEditor.setSelection(index, 1);
+        // 이미지 url
+        url = res.data.url;
 
-  //             quillEditor.clipboard.dangerouslyPasteHTML(
-  //                 index,
-  //                 `<img src=${res.data} alt=${'alt text'} />`
-  //             );
-  //         }
-  //     }
-  // }
+        if (QuillRef.current) {
+          // 현재 Editor 커서 위치에 서버로부터 전달받은 이미지 url을 이용하여 이미지 태그 추가
+          const index = QuillRef.current?.getEditor().getSelection()?.index;
 
-  // const imageHandler = () => {
-  // 	// 파일을 업로드 하기 위한 input 태그 생성
-  //   const input = document.createElement("input");
-  //   const formData = new FormData();
-  //   let url = "";
+          if (index !== null && index !== undefined) {
+            const quill = QuillRef.current?.getEditor();
 
-  //   input.setAttribute("type", "file");
-  //   input.setAttribute("accept", "image/*");
-  //   input.click();
+            quill?.setSelection(index, 1);
 
-  // // 파일이 input 태그에 담기면 실행 될 함수
-  //   input.onchange = async () => {
-  //     const file = input.files;
-  //     if (file !== null) {
-  //       formData.append("image", file[0]);
-
-  // // 저의 경우 파일 이미지를 서버에 저장했기 때문에
-  //   	// 백엔드 개발자분과 통신을 통해 이미지를 저장하고 불러왔습니다.
-  //       try {
-  //         const res = axios를 통해 백엔드 개발자분과 통신했고, 데이터는 폼데이터로 주고받았습니다.
-
-  // // 백엔드 개발자 분이 통신 성공시에 보내주는 이미지 url을 변수에 담는다.
-  //         url = res.data.url;
-
-  // // 커서의 위치를 알고 해당 위치에 이미지 태그를 넣어주는 코드
-  //   	// 해당 DOM의 데이터가 필요하기에 useRef를 사용한다.
-  //         const range = QuillRef.current?.getEditor().getSelection()?.index;
-  //         if (range !== null && range !== undefined) {
-  //           let quill = QuillRef.current?.getEditor();
-
-  //           quill?.setSelection(range, 1);
-
-  //           quill?.clipboard.dangerouslyPasteHTML(
-  //             range,
-  //             `<img src=${url} alt="이미지 태그가 삽입됩니다." />`
-  //           );
-  //         }
-
-  //         return { ...res, success: true };
-  //       } catch (error) {
-  //         const err = error as AxiosError;
-  //         return { ...err.response, success: false };
-  //       }
-  //     }
-  //   };
-  // };
+            quill?.clipboard.dangerouslyPasteHTML(
+              index,
+              `<img src=${res.data.url} alt="이미지 태그가 삽입됩니다." />`
+            );
+          }
+        }
+      } catch (error) {
+        const err = error as AxiosError;
+        return { ...err.response, success: false };
+      }
+    };
+  };
 
   /**
    * quill에서 사용할 모듈을 설정하는 코드
@@ -119,7 +90,7 @@ const MatEditor = ({ htmlContent, setHtmlContent }: QuillEditorProps) => {
           ["image"],
         ],
         handlers: {
-          // image: imageHandler,
+          image: imageHandler,
         },
       },
       imageResize: {
