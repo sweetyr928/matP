8e8460fc83118e42b00cd4776e1d2c748cd53bec
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2023-01-20 11:15:05 +0900

    #34 - feat : aws s3 파일업로드를 위한 컨트롤러 및 검증 클래스 FileUtils 구현
    # : 업로드한 파일관련 validation 처리는 FileUtils 에서 모두 처리함.

---
+++ server/matp/src/main/java/com/matp/s3upload/exception/FileValidatorException.java
@@ -0,0 +1,8 @@
+package com.matp.s3upload.exception;
+
+public class FileValidatorException extends RuntimeException {
+
+    public FileValidatorException(String msg) {
+        super(msg);
+    }
+}
