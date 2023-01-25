d97ae3aa28ec2631eea2251c06b768d512b90592
bonojuno <gtx540@naver.com>
2023-01-25 20:59:59 +0900

    #71 - refactor : 사용하지 않는 코드 삭제 및 코드 변경 사항 적용
    멤버 쪽 코드 변경 사항 
    
    - 기존 이메일에서 id를 사용하여 조회하는 것으로 결정남에 따라
       그에 관련된 코드 변경
    - 사용하지 않는 코드, 주석, 로그 삭제

--- server/matp/src/main/java/com/matp/member/controller/MemberController.java
+++ server/matp/src/main/java/com/matp/member/controller/MemberController.java
@@ -4,11 +4,9 @@ import com.matp.auth.jwt.JwtTokenProvider;
 import com.matp.member.dto.MemberPatchDto;
 import com.matp.member.dto.MemberResponse;
 import com.matp.member.dto.MemberSearchResponse;
-import com.matp.member.repository.MemberRepository;
 import com.matp.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
@@ -25,26 +23,23 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
-    private final MemberRepository memberRepository; // 기능 확인 위해 임시로 추가
     private final MemberService memberService;
     private final JwtTokenProvider jwtTokenProvider;
 
     /**
-     * 유저 정보와 팔로우 정보를 함께 가져온 후 토큰에서 email 추출하여 해당 유저 정보만 반환
-     * TODO: pickerList도 함께 가져와야 함
+     * 유저 정보와 필요한 다른 정보를 함께 가져온 후 토큰에서 id 추출하여 해당 유저 정보만 반환
      */
     @GetMapping("/mypage")
     public Mono<ResponseEntity<MemberResponse>> myPage(ServerHttpRequest request) {
-        String email = extractEmail(request);
+        Long id = extractId(request);
         return memberService.findAllWithInfo()
-                .filter(member -> member.email().equals(email))
+                .filter(member -> member.id().equals(id))
                 .last()
                 .map(ResponseEntity::ok);
     }
 
     /**
-     * 유저 정보와 팔로우 정보를 함께 가져온 후 memberId로 해당 유저 정보만 반환
-     * TODO: pickerList도 함께 가져와야 함
+     * 유저 정보와 필요한 다른 정보를 함께 가져온 후 memberId로 해당 유저 정보만 반환
      */
     @GetMapping("/{member-id}")
     public Mono<ResponseEntity<MemberResponse>> findMember(@PathVariable("member-id") Long id) {
@@ -64,27 +59,20 @@ public class MemberController {
     @PatchMapping
     public Mono<ResponseEntity<MemberPatchDto>> updateMember(@RequestBody MemberPatchDto patchRequest,
                                                              ServerHttpRequest request) {
-        String email = extractEmail(request);
-        return memberService.updateMember(email, patchRequest)
+        Long id = extractId(request);
+        return memberService.updateMember(id, patchRequest)
                 .map(MemberPatchDto::from)
                 .map(ResponseEntity::ok);
     }
 
-    // 기능 확인 위해 임시로 추가
-    @DeleteMapping("/{member-id}")
-    public Mono<ResponseEntity<String>> deleteMember(@PathVariable("member-id") Long id) {
-        return memberRepository.deleteById(id)
-                .then(Mono.just(new ResponseEntity<>("삭제 완료", HttpStatus.NO_CONTENT)));
-    }
-
     /**
-     * 리퀘스트 헤더에 있는 토큰을 사용하여 이메일 추출
+     * 리퀘스트 헤더에 있는 토큰을 사용하여 ID 추출
      */
-    private String extractEmail(ServerHttpRequest request) {
+    private Long extractId(ServerHttpRequest request) {
         String bearerToken = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
         assert bearerToken != null;
         bearerToken = bearerToken.substring(7);
-        return jwtTokenProvider.getUserEmail(bearerToken);
+        return jwtTokenProvider.getUserId(bearerToken);
     }
 
 }
