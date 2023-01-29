f19bacab36de4f8d9a7230093c1f49583ff0cb33
sweetyr928 <031694@naver.com>
2023-01-30 02:08:51 +0900

    #80 - style : post scroll 기능 추가

--- client/src/pages/MatPeoplePage.tsx
+++ client/src/pages/MatPeoplePage.tsx
@@ -12,8 +12,6 @@ import useAxios from "../hooks/useAxios";
 import { IPosts } from "../api/axiosAPI/posts/PostsAxios";
 import PostRead from "../components/PostRead";
 
-const jwtToken = localStorage.getItem("Authorization");
-
 const FeedContainer = styled.div`
   height: 100%;
   min-width: calc(1340px * 2 / 5 - 63px);
@@ -113,25 +111,28 @@ const StyledPosts = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(110px, 1fr));
   grid-gap: 4px;
   margin: 0px 0px 0px 0px;
+
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const MatPeople: React.FC = () => {
   const { id } = useParams();
-
   const [followReload, setFollowReload] = useState<boolean>(false);
 
-  const navigate = useNavigate();
-
   useEffect(() => {
-    console.log(followReload);
     getMatPeopleInfo();
   }, [followReload]);
 
-  const { axiosData: getMatPeopleInfo, responseData: matPeople } = useAxios(
-    () => getMatPeople(Number(id)),
-    [],
-    false
-  );
+  const navigate = useNavigate();
+
+  const {
+    axiosData: getMatPeopleInfo,
+    responseData: matPeople,
+    status,
+  } = useAxios(() => getMatPeople(Number(id)), [followReload], false);
 
   const { axiosData: follow } = useAxios(
     () => followMatPeople(Number(id)),
@@ -157,12 +158,14 @@ const MatPeople: React.FC = () => {
   } = matPeople || {};
 
   const handleFollow = () => {
-    if (isFollowing) {
-      unfollow();
-      setFollowReload(!followReload);
-    } else {
-      follow();
-      setFollowReload(!followReload);
+    if (status === "Idle" || status === "Success") {
+      if (isFollowing) {
+        unfollow();
+        setFollowReload(!followReload);
+      } else {
+        follow();
+        setFollowReload(!followReload);
+      }
     }
   };
 
